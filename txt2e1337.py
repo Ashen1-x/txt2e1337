33c0d0ce8da6c91b34cc6ad677665ca4216582bf
Ashen1 <beelzabul@proton.me>
2025-03-09 22:43:12 -0700

    wrote the code, and added a txt file to put what you want translated in

---
+++ txt2e1337.py
@@ -0,0 +1,21 @@
+normalAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+translatedAlphabet = ["4", "8", "(", "<|", "3", "ph", "6", "|-|", "|", "J", "|<", "1", "|\/|", "|\|", "0", "|2", "Q","R", "5", "7", "|_|", "\/", "\/\/", "><", "'/","Z"]
+
+translator = dict(zip(normalAlphabet, translatedAlphabet))
+
+with open("message.txt", 'r') as file:
+			words = file.read()
+			
+translated_text = "".join(
+    translator.get(char.lower(), char).upper() if char.isupper() else translator.get(char, char)
+    for char in words
+)
+                
+		
+
+
+print(words)
+print(translated_text)
+
+## if/else statement for if in nA switch with tA else print i
+
